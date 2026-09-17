5728a6187e8467aaae79bf183c3fd03fd4358740
wwSlam <pvmmach@gmail.com>
2026-09-17 17:23:21 +0300

    Lab03:compound assignment operators

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Банковский счет");
+
+double balance = 1000;
+Console.WriteLine($"Начальный баланс: {balance}");
+
+balance += 500; //пополнение
+Console.WriteLine($"После пополнения на 500: {balance}");
+
+balance -= 200; //покупка
+
+Console.WriteLine($"После покупки на 200: {balance}");
+
+balance *= 1.05; //начисление 5% процентов
+Console.WriteLine($"После начисления 5%: {balance}");
+
+balance /= 2; //разделили счёт пополам с партнёром
+Console.WriteLine($"После деления пополам: {balance}");
+
